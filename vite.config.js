eaed5009a00ca1e8f771f8d5ac652538acfc789b
Rajdeep Ghosh <ghoshrajdeep2000@gmail.com>
2026-09-19 22:07:10 +0530

    end points updated

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+const API = 'http://134.209.215.255:8000'
+const api = { target: API, changeOrigin: true }
+
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    // proxy: {
+    //   '/survey/check':       'http://localhost:8000',
+    //   '/survey/start':       'http://localhost:8000',
+    //   '/survey/progress':    'http://localhost:8000',
+    //   '/survey/verify-code': 'http://localhost:8000',
+    //   '/survey/submit':      'http://localhost:8000',
+    //   '/healthz':            'http://localhost:8000',
+    // }
+
     proxy: {
-      '/survey/check':       'http://localhost:8000',
-      '/survey/start':       'http://localhost:8000',
-      '/survey/progress':    'http://localhost:8000',
-      '/survey/verify-code': 'http://localhost:8000',
-      '/survey/submit':      'http://localhost:8000',
-      '/healthz':            'http://localhost:8000',
-    }
+      '/survey/check': api,
+      '/survey/start': api,
+      '/survey/progress': api,
+      '/survey/verify-code': api,
+      '/survey/submit': api,
+      '/healthz': api,
+    },
   }
 })
+
+
+
+
+
+
